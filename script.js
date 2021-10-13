d374b75e9c39191f89b710ca747888272bf7af92
Ajeet Singh <80536741+AjeetSingh2016@users.noreply.github.com>
2021-10-13 21:44:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let howToPlay = document.querySelector('.htpbutton');
+
+howToPlay.addEventListener('click',function(){
+    alert("When a player reaches a total of 100 or more points, the game ends and that player is the winner.");
+})
