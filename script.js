c5f2974340a4b007d3c15ff2815bac1c14588c88
Ajeet Singh <2016appsingh@gmail.com>
2021-10-16 08:03:00 +0530

    Changes

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+
+
+
+
+
+
+
+
+
 let howToPlay = document.querySelector('.htpbutton');
+const rollDice = document.querySelector('.roll-dice');
+let livescore = document.querySelector('.livescore');
 
 howToPlay.addEventListener('click',function(){
     alert("When a player reaches a total of 100 or more points, the game ends and that player is the winner.");
 })
+
+let num;
+rollDice.addEventListener('click',function(){
+    num = Math.floor(Math.random() * 6)+1;
+    livescore.textContent = num;   
+})
+// random();
+// console.log(num);
